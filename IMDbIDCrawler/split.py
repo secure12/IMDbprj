e5e36c8cf6f1da351f4f4aa9a5c86ff7c2bd0b67
trbhei <h.hei@hotmail.com>
2016-10-31 00:26:40 +0800

    Revert "Finished"
    This reverts commit 4aec5cec3f0f72e24ce771b7e851379a679eafb8.

--- IMDbIDCrawler/split.py
+++
@@ -1,8 +0,0 @@
-f = open("movie_ids06-15", 'r')
-l = []
-id_count = 0
-for line in f:
-    if id_count % 10000 == 0:
-        l.append(open("movie_ids06-15." + str(len(l)), 'w'))
-    id_count += 1
-    l[-1].write(line)
--- IMDb_data/make.py
+++
@@ -1,3 +0,0 @@
-import py_compile
-for i in range(8):
-    py_compile.compile("get_movie_data_"+ str(i)+".py")
